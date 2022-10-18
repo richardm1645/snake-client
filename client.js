b121e806f3fad992b586980d519a83e36780ca3a
Richard Miao <richardmiao1645@gmail.com>
2022-10-18 15:22:47 -0700

    First iteration of move commands

--- client.js
+++ client.js
@@ -1,7 +1,7 @@
 const connect = function () {
   const net = require("net");
   const conn = net.createConnection({
-    host: "172.31.121.116",
+    host: "172.31.113.119",
     port: "50541"
   });
 
@@ -10,9 +10,16 @@ const connect = function () {
   conn.on("connect", () => {
     console.log("Successfully connected to game server");
     conn.write("Name: SNK");
+    /*
+    setTimeout( () => {
+      conn.write("Move: up")
+    }, 1000)
+    */
   });
 
   conn.on("data", () => { 
+    
+    
     console.log("You ded cuz you idled");
   });
 
