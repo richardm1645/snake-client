9713927f6de93f79e226127469225e861af378b7
Richard Miao <richardmiao1645@gmail.com>
2022-10-17 20:30:54 -0700

    Add name upon connection to server

--- client.js
+++ client.js
@@ -5,10 +5,15 @@ const connect = function () {
     port: "50541"
   });
 
-  conn.setEncoding("utf8");
+  conn.setEncoding("utf8"); //Interpret data as text
+
+  conn.on("connect", () => {
+    console.log("Successfully connected to game server");
+    conn.write("Name: SNK");
+  });
 
   conn.on("data", () => { 
-    console.log("You ded cuz you idled")
+    console.log("You ded cuz you idled");
   });
 
   return conn;
